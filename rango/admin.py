da26114b06733a993ea51613223075be7e1e845b
Daryna Darii <2916076d@student.gla.ac.uk>
2025-02-21 23:20:29 +0200

    chapter 5 (exercises - part 2)

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-#adding classes to include them to the admin interface
-admin.site.register(Category)
-admin.site.register(Page)
+#admin.ModelAdmins customizes how a model appears in the Django admin panel.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url') 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',) 
+
+# Register models with custom admin views
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
